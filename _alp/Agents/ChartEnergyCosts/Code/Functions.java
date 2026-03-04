766266b75347f56d96342d1bb84288841fe0715c
Ate Hempenius <ate@zenmo.com>
2026-03-04 12:39:39 +0100

    initial updates content economic charts

--- _alp/Agents/ChartEnergyCosts/Code/Functions.java
+++ _alp/Agents/ChartEnergyCosts/Code/Functions.java
@@ -152,52 +152,6 @@ bar_exportElectricityRevenueMonthly.removeAll();
 plot_netElectricityCostsMonthly.removeAll();
 /*ALCODEEND*/}
 
-double f_calculateCapacityCosts_eur(double[] netLoad_kW)
-{/*ALCODESTART::1755868971285*/
-double costsCapacityRate_euro = 0;
-
-GridNode GN_T0 = findFirst(uI_Results.energyModel.pop_gridNodes, p -> p.p_gridNodeID.equals("T0"));
-double contractedCapacity_kW = GN_T0.p_capacity_kW;
-double VAT_fr = 0.21;
-double annualConnectionRate_euro_p_yr = 5351;
-double annualFixedTransportRate_euro_p_yr = 2760;
-double annualContractCapacityRate_euro_p_kW_yr = 42.10;
-double monthlyPeakPowerRate_euro_p_kW_month = 4.48;
-    	
-double[] monthlyPeakDemand_kW = f_calculateMonthlyPeakDemand_kW(netLoad_kW);
-    
-costsCapacityRate_euro = (1+VAT_fr)*(annualConnectionRate_euro_p_yr + annualFixedTransportRate_euro_p_yr + annualContractCapacityRate_euro_p_kW_yr * contractedCapacity_kW + monthlyPeakPowerRate_euro_p_kW_month * Arrays.stream(monthlyPeakDemand_kW).sum());
-
-return costsCapacityRate_euro;
-/*ALCODEEND*/}
-
-double[] f_calculateMonthlyPeakDemand_kW(double[] netLoad_kW)
-{/*ALCODESTART::1755869067049*/
-double[] monthlyPeakDemand_kW = new double[12];
-int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-int sampleCounter = 0;
-    	
-for(int month=0; month < daysInMonth.length; month++) {
-    
-    double maxLoad_kW = 0;
-    
-    int samplesInMonth = daysInMonth[month] * 96;
-    int startMonthIndex = sampleCounter;
-    int endMonthIndex = sampleCounter + samplesInMonth;
-    		
-	for (int i = startMonthIndex; i < endMonthIndex && i < netLoad_kW.length; i++) {
-    	double absLoad_kW = Math.abs(netLoad_kW[i]);
-        if (absLoad_kW > maxLoad_kW) {
-        	maxLoad_kW = absLoad_kW;
-        }
-    }
-    monthlyPeakDemand_kW[month] = maxLoad_kW;
-   	sampleCounter += samplesInMonth;
-}
-	
-return monthlyPeakDemand_kW;
-/*ALCODEEND*/}
-
 double[] f_calculateMonthlyNetElectricityCosts_eur(double[] monthlyElectricityImportCosts_euro,double[] monthlyElectricityExportRevenue_euro)
 {/*ALCODESTART::1755869113668*/
 if(monthlyElectricityImportCosts_euro.length != monthlyElectricityExportRevenue_euro.length){
